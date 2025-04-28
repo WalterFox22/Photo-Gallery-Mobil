d7b7ec31ac67c96f96391ee0b3d019a456333ec2
Walter <john.cobacango@epn.edu.ec>
2025-04-28 17:57:18 -0500

    Guardar las fotos, edicion en phot.service.ts,  tab2.page.html

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,20 +1,74 @@
 import { Injectable } from '@angular/core';
-import {Camera, CameraResultType, CameraSource, Photo} from '@capacitor/camera'
-import {Filesystem, Directory} from '@capacitor/filesystem'
-import {Preferences} from '@capacitor/preferences'
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
+import { WebView } from '@capacitor/core';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhotoService {
+  public photo: UserPhoto[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  public async addNewToGallery(){
-    const capturedPhoto=await Camera.getPhoto({
-      resultType:CameraResultType.Uri,
-      source:CameraSource.Camera,
-      quality:100
+  private async savePicture(photo:Photo){
+    const base64Data= await this.readAsBase64(photo);
+    const fileName = Date.now() + '.jpeg';
+    const saveFile= await Filesystem.writeFile({
+      path:fileName,
+      data:base64Data,
+      directory:Directory.Data,
     })
+
+    return{
+      filepath: fileName,
+      webviewPath:photo.webPath
+    }
   }
+
+  private async readAsBase64 (photo:Photo){
+    const response= await fetch(photo.webPath!)
+    const blob = await response.blob()
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+  
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
+
+
+
+
+  public async addNewToGallery() {
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+
+    // Para guarda la imagen
+     const saveImageFile= await this.savePicture(capturedPhoto)
+     this.photo.unshift(saveImageFile)
+
+
+
+  }
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
 }
