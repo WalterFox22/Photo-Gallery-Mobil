e7cefee119dc54241bf7be696884c4466deafb0c
WALTER\Soledad Cobacango <walteracero005@gmail.com>
2025-05-18 00:44:24 -0500

    Creacion de Spash Screen, installacion y configuracion de archivos android y assets

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,26 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'photo-gallery',
-  webDir: 'www'
+  appId: 'epn.myGallery.ec',
+  appName: 'Gallery FoxDev',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#007BFF",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: false,
+      splashImmersive: false,
+      layoutName: "launch_screen",
+      useDialog: false,
+    },
+  },
 };
 
 export default config;
